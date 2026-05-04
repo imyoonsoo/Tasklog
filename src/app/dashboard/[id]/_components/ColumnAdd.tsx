f84c371fd36d234d27f48be5e52efdf5c97a3cd9
chaemin58 <sulv0309@naver.com>
2026-05-05 00:56:28 +0900

    feat: 컬럼 링크 연결

--- src/app/dashboard/[id]/_components/ColumnAdd.tsx
+++ src/app/dashboard/[id]/_components/ColumnAdd.tsx
@@ -9,7 +9,7 @@ import plusBtn from "@/assets/common/ic-plus.svg";
 export function ColumnAdd() {
   const { id } = useParams();
   return (
-    <Link href={`/${id}/column-add`}>
+    <Link href={`/dashboard/${id}/column-add`}>
       <div className="bg-black-700 flex h-15 min-w-80 gap-2 rounded-[18px] px-3.5 py-4">
         <Image src={plusBtn} alt="새로운 컬럼 추가 버튼" />
         <div className="pl-1 text-[18px] font-medium text-gray-400">
