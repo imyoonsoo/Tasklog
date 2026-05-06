8acb59b8787094ff07cd008c8835e0bc00bfff63
Sung_yuseung <usngsung0830@naver.com>
2026-05-06 12:59:41 +0900

    fix: edit 모달과 page 두개 중 모달만 남기기

--- src/app/test/delete/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Delete() {
-  return (
-    <Link href="/column-delete">
-      아아아아아아아아아아아아아아아아아아아아아아아아아아
-    </Link>
-  );
-}
--- src/app/test/side-menu/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SideMenu } from "@/components/SideMenu";
-
-export default function SideMenuPage() {
-  return (
-    <div>
-      <SideMenu />
-    </div>
-  );
-}
