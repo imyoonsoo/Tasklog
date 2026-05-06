458d19995a68f95f1b0d3762de221f195927742e
imyoonsoo <bliss38soo@gmail.com>
2026-05-06 14:14:22 +0900

    fix: 실행 오류 수정

--- src/components/input/input-field.tsx
+++ src/components/input/input-field.tsx
@@ -1,30 +1,36 @@
+import { ComponentPropsWithRef } from "react";
+
 import { cn } from "@/lib/cn";
 
 import { useInputContext } from "./input-context";
-import { InputWrapperStyles } from "./input-style";
+import { InputWrapperStylesProps } from "./input-style";
 
-interface InputWrapperProps {
-  children: React.ReactNode;
-  className?: string;
-}
+interface InputFieldProps
+  extends
+    ComponentPropsWithRef<"input">,
+    Omit<InputWrapperStylesProps, "error" | "disabled"> {}
 
 /**
- * @description Input 컴포넌트의 Wrapper
- * @param {React.ReactNode} children - Input 컴포넌트의 자식 요소
- * @param {string} className - Input.Wrapper의 클래스 이름
- * @returns {JSX.Element} Input.Wrapper 컴포넌트
+ * @description Input.Field
+ * @param {string} className - Input.Field의 클래스 이름
+ * @returns {JSX.Element} Input.Field 컴포넌트
  */
-export function InputWrapper({ children, className }: InputWrapperProps) {
-  const { inputSize, errorMessage, isDisabled } = useInputContext();
+export function InputField({ className, type, ...props }: InputFieldProps) {
+  const { isPasswordVisible, isDisabled } = useInputContext();
+
+  // 비밀번호 타입일 경우 토글 상태에 따라 실제 type 결정
+  const inputType = type === "password" && isPasswordVisible ? "text" : type;
+
   return (
-    <div
+    <input
+      type={inputType}
+      disabled={isDisabled ?? props.disabled}
       className={cn(
-        InputWrapperStyles({ inputSize, error: !!errorMessage, isDisabled }),
-        "autofill-wrapper",
+        "w-full bg-transparent text-gray-300 outline-none placeholder:text-gray-500 disabled:cursor-not-allowed disabled:text-gray-400",
+        "autofill-field",
         className
       )}
-    >
-      {children}
-    </div>
+      {...props}
+    />
   );
 }
